303028d0deef2cba083d0ca55afe97f71f1be60d
ridham111 <90979646+ridham111@users.noreply.github.com>
2023-07-27 01:49:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
 const searchInput = document.getElementById('searchInput');
 const mealResultsDiv = document.getElementById('mealResults');
-
+favoriteMeals = [];
 
 // Function to fetch meals from TheMeal API based on search input
 async function fetchMeals(query) {
@@ -11,72 +11,100 @@ async function fetchMeals(query) {
 }
 
 // Function to render search results on the Home Page
-// Function to render search results on the Home Page
 var meals__global= [];
 function renderSearchResults(meals) {
   meals__global = meals;
-  console.log(meals,meals__global);
   mealResultsDiv.innerHTML = '';
   meals.forEach(meal => {
     const mealCard = document.createElement('div');
     mealCard.classList.add('meal-card');
     mealCard.innerHTML = `
-      <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+      <img src="${meal.strMealThumb}" width="200" height="200" alt="${meal.strMeal}">
       <h3>${meal.strMeal}</h3>
-
-      <button class="favorite-btn" onclick="addToFavorites('${meal.idMeal}')">Add to Favorites</button>
+      <div style = 'display : flex; '>
       <button class="detail-btn" id="${meal.idMeal}" onclick="showMealDetails('${meal.idMeal}')">Details</button>
-    `;
+      
+        <span class="fav-icon" id ="fav${meal.idMeal}" onclick="addToFavorites('${meal.idMeal}')"><i class="fa-regular fa-heart fa-2x"></i></span>
+      </div>
+
+      `;
+
     mealResultsDiv.appendChild(mealCard);
   });
+  // if already store in favorite then change favorite button
+  addRemove_button();
 }
 
-
-// Function to handle adding meals to favorites
-function addToFavorites(mealId) {
-console.log(mealId,meals__global);
-  const mealToAdd = meals__global.find(meal => meal.idMeal === mealId);
-  if (mealToAdd && !favoriteMeals.some(meal => meal.idMeal === mealId)) {
-    favoriteMeals.push(mealToAdd);
-    updateFavoritesUI();
-    saveFavoritesToLocalStorage();
-  }
-}
-
-// Function to update the UI with favorite meals
-function updateFavoritesUI() {
-  favoriteMealsDiv.innerHTML = '';
-  favoriteMeals.forEach(meal => {
-    const favoriteCard = document.createElement('div');
-    favoriteCard.classList.add('meal-card');
-    favoriteCard.innerHTML = `
-      <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-      <h3>${meal.strMeal}</h3>
-      <button class="remove-btn" onclick="removeFromFavorites('${meal.idMeal}')">Remove from Favorites</button>
-    `;
-    favoriteMealsDiv.appendChild(favoriteCard);
+function addRemove_button(){
+  meals__global.forEach(meal => {
+    if(favoriteMeals.some(meal1 => meal1.idMeal === meal.idMeal)){
+      const favelem = document.getElementById('fav'+meal.idMeal);
+      console.log(favelem);
+      favelem.lastChild.classList.remove('fa-regular');
+      favelem.lastChild.classList.add('fa-solid')
+    }
   });
 }
 
+async function renderAllMeals() {
+  const meals = await fetchMeals('');
+  setHeaderText( 'All Meals');
+  renderSearchResults(meals);
+}
 
-// ... (rest of the code)
+
+// Event listener for initial load of the app
+document.addEventListener('DOMContentLoaded', renderAllMeals);
+function setHeaderText(text) {
+  const header = document.getElementById('header');
+  header.textContent = text;
+}
 
 // Event listener for search input
 searchInput.addEventListener('input', async (e) => {
   const query = e.target.value.trim();
-  if (query.length > 0) {
+  setHeaderText(query.length === 0 ? 'All Meals' : 'Search Results');
     const meals = await fetchMeals(query);
     renderSearchResults(meals);
-  } else {
-    mealResultsDiv.innerHTML = '';
-  }
 });
 
 function goToFavorites() {
   window.location.href = 'favorite/favorites.html';
 }
 
+// Function to handle adding meals to favorites
+function addToFavorites(mealId) {
+    const mealToAdd = meals__global.find(meal => meal.idMeal === mealId);
+    if (mealToAdd && !favoriteMeals.some(meal => meal.idMeal === mealId)) {
+      favoriteMeals.push(mealToAdd);
+      saveFavoritesToLocalStorage();
+      const favelem = document.getElementById('fav'+mealId);
+      favelem.lastChild.classList.remove('fa-regular');
+      favelem.lastChild.classList.add('fa-solid')
+    }
+    else{
+      favoriteMeals = favoriteMeals.filter(meal => meal.idMeal !== mealId);
+      saveFavoritesToLocalStorage();
+      const favelem = document.getElementById('fav'+mealId);
+      favelem.lastChild.classList.remove('fa-solid')
+      favelem.lastChild.classList.add('fa-regular')
+    }
+  }
+
+  function updateremovebtnUI(mealid){
+    const favelem = document.getElementById('fav'+mealid);
+    favelem.lastChild.classList.remove('fa-regular');
+    favelem.lastChild.classList.add('fa-solid');
+  }
 
 function saveFavoritesToLocalStorage() {
   localStorage.setItem('favoriteMeals', JSON.stringify(favoriteMeals));
+}
+
+// Initialization: Load favorite meals from localStorage if available
+if (localStorage.getItem('favoriteMeals')) {
+  favoriteMeals = JSON.parse(localStorage.getItem('favoriteMeals'));
+  favoriteMeals.forEach(element => {
+    const fav_element = document.getElementById('fav'+(element.idMeal))
+  });
 }
