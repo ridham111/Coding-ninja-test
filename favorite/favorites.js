49f7d48322e2438910a1bd53281e21e56a848548
ridham111 <90979646+ridham111@users.noreply.github.com>
2023-07-27 01:49:06 +0530

    Update favorites.js

--- favorite/favorites.js
+++ favorite/favorites.js
@@ -9,8 +9,8 @@ function updateFavoritesUI() {
     const favoriteCard = document.createElement('div');
     favoriteCard.classList.add('meal-card');
     favoriteCard.innerHTML = `
-      <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-      <h3>${meal.strMeal}</h3>
+    <img src="${meal.strMealThumb}" width="200" height="200" alt="${meal.strMeal}">
+    <h3>${meal.strMeal}</h3>
       <button class="remove-btn" onclick="removeFromFavorites('${meal.idMeal}')">Remove from Favorites</button>
     `;
     favoriteMealsDiv.appendChild(favoriteCard);
@@ -42,8 +42,14 @@ function saveFavoritesToLocalStorage() {
 // Initialization: Load favorite meals from localStorage if available
 if (localStorage.getItem('favoriteMeals')) {
   favoriteMeals = JSON.parse(localStorage.getItem('favoriteMeals'));
-  updateFavoritesUI();
+  favoriteMeals.forEach(element => {
+    console.log(element.idMeal,'fav'+(element.idMeal));
+    const fav_element = document.getElementById('fav'+(element.idMeal))
+    console.log(fav_element);
+
+  });
 }
+document.addEventListener('DOMContentLoaded', updateFavoritesUI);
 
 
 function goToSearch(){
